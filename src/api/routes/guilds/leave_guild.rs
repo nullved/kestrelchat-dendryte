68b09a9d6855502045db4b09634ee062f4ecd79a
nullved <nullved19@gmail.com>
2026-06-28 00:58:56 -0500

    add join and test endpoints, guild test

---
+++ src/api/routes/guilds/leave_guild.rs
@@ -0,0 +1,46 @@
+use crate::api::guards::auth_context::AuthContext;
+use crate::api::guards::rate_limit::WithinRateLimit;
+use crate::database::postgres::{
+    connection::Database,
+    operations::guilds::join_guild as postgres_join_guild,
+};
+use crate::errors::AppError;
+use chrono::{DateTime, Utc};
+use rocket::serde::json::Json;
+use rocket::State;
+use rocket_okapi::openapi;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, JsonSchema)]
+pub struct JoinGuildRequest {
+    pub guild_id: String,
+}
+
+#[derive(Serialize, JsonSchema)]
+pub struct JoinGuildResponse {
+    pub guild_id: String,
+    pub user_id: String,
+    pub joined_at: DateTime<Utc>
+}
+
+#[openapi(tag = "Guilds")]
+#[post("/join", data="<req>")]
+pub async fn join_guild(
+    _within_rate_limit: WithinRateLimit,
+    postgres: &State<Database>,
+    auth_ctx: AuthContext,
+    req: Json<JoinGuildRequest>
+) -> Result<Json<JoinGuildResponse>, AppError> {
+    let user_id = auth_ctx.user_id;
+
+    let guild_member = postgres_join_guild(postgres, &req.guild_id, &user_id)
+        .await
+        .map_err(AppError::from)?;
+
+    Ok(Json(JoinGuildResponse {
+        guild_id: guild_member.guild_id,
+        user_id: guild_member.user_id,
+        joined_at: guild_member.joined_at
+    }))
+}
---
+++ src/database/postgres/operations/guilds/leave_guild.rs
@@ -0,0 +1,29 @@
+use crate::database::postgres::connection::Database;
+use crate::database::postgres::error::DatabaseError;
+use chrono::Utc;
+use sqlx::query_as;
+use crate::models::GuildMember;
+
+pub async fn join_guild(
+    postgres: &Database,
+    guild_id: &str,
+    user_id: &str
+) -> Result<GuildMember, DatabaseError> {
+    let now = Utc::now();
+
+    let guild_member = query_as::<_, GuildMember>(
+        r#"
+        INSERT INTO guild_members (guild_id, user_id, joined_at)
+        VALUES ($1, $2, $3)
+        RETURNING guild_id, user_id, joined_at
+        "#,
+    )
+    .bind(&guild_id)
+    .bind(user_id)
+    .bind(now)
+    .fetch_one(postgres.pool())
+    .await
+    .map_err(DatabaseError::from_sqlx)?;
+
+    Ok(guild_member)
+}
